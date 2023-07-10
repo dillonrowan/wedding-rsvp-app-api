9e0da15a03dcc2b553d61b8acce9b378e708168f
Dillon Rowan <rowan.dillon@heb.com>
2023-07-09 19:16:28 -0500

    add comments to cors class

--- src/main/java/com/dillon/weddingrsvpapi/config/WebConfig.java
+++ src/main/java/com/dillon/weddingrsvpapi/config/WebConfig.java
@@ -6,13 +6,24 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+/**
+ * Web configuration class.
+ */
 @Configuration
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 
+    /**
+     * The allowed origin to make requests.
+     */
     @Value("${allowed.origin}")
     private String allowedOrigin;
 
+    /**
+     * Configures CORS policy. Adds "POST", "GET", "PUT", "PATCH", "DELETE", "OPTIONS" as allowed request methods.
+     * Sets the allowed origin to the value in {@link allowedOrigin}.
+     * @param registry
+     */
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")

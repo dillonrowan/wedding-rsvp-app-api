5fa75f8b2fd529e58fa9918e529bdf7ea901d973
Dillon Rowan <rowan.dillon@heb.com>
2023-09-23 16:15:27 -0500

    add additional data to the RsvpGroupRepositoryTest

--- src/test/groovy/com/dillon/weddingrsvpapi/db/RsvpGroupRepositorySpec.groovy
+++ src/test/groovy/com/dillon/weddingrsvpapi/db/RsvpGroupRepositorySpec.groovy
@@ -21,6 +21,7 @@ class RsvpGroupRepositorySpec extends Specification {
 
     def 'When a valid name is provided, the group and its members with that member name is returned'() {
         given:
+        // expected group and members to return
         def rsvp = Rsvp.builder()
             .id(1)
             .attending(false)
@@ -41,6 +42,28 @@ class RsvpGroupRepositorySpec extends Specification {
             .rsvps(Set.of(rsvp, rsvpTwo))
             .groupLead("John Smith").build()
         rsvpGroupRepository.save(rsvpGroup)
+
+        // group of members whose nobody's name is similar to the provided name
+        def rsvpThree = Rsvp.builder()
+            .id(3)
+            .attending(false)
+            .name("Micheal James").build()
+        rsvpRepository.save(rsvpThree)
+        def rsvpFour = Rsvp.builder()
+            .id(4)
+            .attending(false)
+            .name("Gracie James").build()
+        rsvpRepository.save(rsvpFour)
+
+        def rsvpGroupTwo = RsvpGroup.builder()
+            .id(2)
+            .dietaryRestrictions(List.of( DietaryRestriction.NO_PORK ))
+            .foodAllergies(List.of( FoodAllergies.DAIRY ))
+            .email("test@test.com")
+            .modifyGroup(false)
+            .rsvps(Set.of(rsvpThree, rsvpFour))
+            .groupLead("Gracie James").build()
+        rsvpGroupRepository.save(rsvpGroupTwo)
 
         when:
         List<RsvpGroup> rsvpGroupRet = rsvpGroupRepository.findAllByRsvpsName("jOhN")
